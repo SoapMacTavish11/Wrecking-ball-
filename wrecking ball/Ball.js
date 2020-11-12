5c829564f8ff9061a5ae3710eea8e5f0d92d93ce
Rudrarachit <rudrarachit@gmail.com>
2020-11-12 16:56:38 +0530

    recreated the ball

--- wrecking ball/Ball.js
+++ wrecking ball/Ball.js
@@ -1,21 +1,25 @@
-class Ball {
-  constructor(x, y, width, height, angle) {
-    var options = { 
-      density: 1, 
-      frictionAir: 0.005};
-
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
-    World.add(world, this.body);
+class Ball{
+constructor(x,y,r){
+  var options ={
+    density: 1,
+    frictionAir:0.005,
+  }
+    this.body=Bodies.circle(x,y,r,options);
+    this.r=r;
+    World.add(world,this.body);
   }
 
-  display() {
-    var angle = this.body.angle;
-    push();
-    translate(this.body.position.x, this.body.position.y);
-    rotate(angle);
-    ellipse(0, 0, this.width, this.height);
-    pop();
-  }
+display(){
+ var angle = this.body.angle;
+ var pos = this.body.position;
+
+push()
+translate(pos.x,pos.y);
+rotate(angle);
+ellipseMode(CENTER);
+ellipse(0,0,this.r,this.r);
+pop()
+}
+
+
 }
